d1b2b436f4afce9c64925ae595fc5453595840c7
Daniel <studiosdopplermusic@gmail.com>
2022-04-27 21:33:49 -0500

    first commit

---
+++ Notas/intro/1_jsx.jsx
@@ -0,0 +1,46 @@
+// JSX
+
+    //Nos permite escribir en JS como se verán los componentes usando una estructura similar a html
+    // Combinación de HTML con JS 
+    
+    // VENTAJAS 
+        // 1. Estructuras de componenentes mas faciles de visualizar.
+        // 2. Errores y adverntecias mas utiles.
+
+    // JSX ES OPCIONAL PERO ES LO MAS RECOMENDADO
+    const nombre = <h1>Hola Mundo!</h1>
+
+
+    // ELEMENTOS...................
+
+        // Unidades más pequeñas en react, definen lo que se ve en pantalla
+        // Los COMPONENTES EN REACT están hechos de elementos
+        // Los elementos en React se representan con letras minusculas 
+
+    // COMPONENTES
+        
+        //Siempre comienzan con una letra mayuscula 
+        
+    // ATRIBUTOS
+
+        // se pueden agregar atributos a las etiquetas HTML, pero se pueden escribir de forma distinta, por ejemplo las clases en JSX se llaman mediante className, esos nombres se cambian debido a algunas palabras reservadas de JS, for por ejemplo (de etiqueta se llama) htmlFor, 
+
+        // estilos
+            // Los estilos deben ser escritos en camelCase por ejemplo CSS = background-image JSX backgroudImage
+
+
+    // Las llaves en React nos permite decirle que es codigo JS
+
+    // REACT DOM 
+
+        // Paquete que facilita la interacción y actualización del DOM en aplicaciones React 
+
+    // DOM 
+
+        // Representación de todos los elementos que conforman una pagina o aplicación web 
+
+    // NOTAS
+        // _todos los elementos de HTML pueden ser usados en JSX
+        
+
+// 45:49
