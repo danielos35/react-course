50ff6c0ddc4cf617a0cf15d76d24f26d672f9e70
Daniel <studiosdopplermusic@gmail.com>
2022-04-29 13:55:43 -0500

    PrimerComponente

---
+++ A_apps/alon/src/componentes/testimonio.js
@@ -0,0 +1,21 @@
+// 1. Paso # 1 importar React 
+import React from "react";
+
+function Testimonio(){
+  return(
+    <div className="contenedorPrincipal">
+      <img 
+        className="imagen"
+        src={"../imagenes/hero.png"}
+        alt="fotoEma"/>
+
+        <div className="textoTestimonio">
+          
+
+        </div>
+    </div>
+  )
+}
+
+
+export default Testimonio
